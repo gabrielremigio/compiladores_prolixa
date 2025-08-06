fd0f845aab6add0a7aebe537e47cd90417358c83
João Vitor Felix <joao.flx@academico.ufs.br>
2025-08-06 15:33:02 -0300

    alterações parciais nas produções 2

--- Etapa3/src/prolixa/Main.java
+++ Etapa3/src/prolixa/Main.java
@@ -13,16 +13,6 @@ public class Main
 		{
 			String arquivo = "/home/john/Códigos/Ambiente-Java/compiladores_prolixa/Etapa3/teste/teste.prlx";
 
-
-			// Teste do Parser
-			Parser parser = new Parser(
-								new Lexer(
-									new PushbackReader(  
-											new FileReader(arquivo), 1024))); 
-											// new InputStreamReader(System.in), 1024)));
-			Start tree = parser.parse();
-
-
 			// // Teste do Lexer
 			// Lexer lexer = new Lexer(
 			// 					new PushbackReader(  
@@ -33,6 +23,15 @@ public class Main
 			// 	System.out.println(token.getClass());
 			// 	System.out.println("( " + token.toString() + ")");
 			// }
+
+			// Teste do Parser
+			Parser parser = new Parser(
+								new Lexer(
+									new PushbackReader(  
+											new FileReader(arquivo), 1024))); 
+											// new InputStreamReader(System.in), 1024)));
+			Start tree = parser.parse();
+
 
 			}
 		catch(Exception e)
